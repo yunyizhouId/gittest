da8becc7d2c516aa6d2571946cc0e99d43aebeca
YUN YIZHOU <yunyizhou777@gmail.com>
2021-06-28 22:58:36 +0900

    add ozisan wo deredere ni shitai

--- app/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Image } from 'react-native';
-import LogoIcon from '../assets/logo.png';
-
-const Logo = () => {
-  return <Image source={LogoIcon} />;
-};
-
-export default Logo;
--- app/resources/colors.js
+++
@@ -1,8 +0,0 @@
-export const white = '#FFFFFF';
-export const gray = '#4A4A4A';
-export const lightGray = '#BDBDBD';
-export const dark = '#19191A';
-export const red = '#F25252';
-export const blue = '#5E9BF1';
-export const primaryTextColor = '#0D0000';
-export const secondaryTextColor = '#BDBDBD';
